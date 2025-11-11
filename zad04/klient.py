6d21d0f0f16f068090466e74b3ba2edc606ff117
Polcia <witkowska.pola@gmail.com>
2025-11-11 11:53:23 +0100

    zadanie 4 finish

--- zad04/klient.py
+++ zad04/klient.py
@@ -1,8 +1,25 @@
 import os
+import errno
+import time
 
-fifo_path = "/tmp/kolejka"
 
-with open(fifo_path, "r") as fifo:
-    print("Odebrano:")
-    for line in fifo:
-        print(line.strip())
+SERVER = "/tmp/kolejka"
+CLIENT = "/tmp/klient"
+
+try:
+    os.mkfifo(CLIENT)
+    print("FIFO klienta utworzone.")
+except OSError as oe:
+    if oe.errno != errno.EEXIST:
+        raise
+
+ID = input("Podaj ID, które chcesz wyszukać: ")
+
+with open(SERVER, "w") as fifo:
+    fifo.write(f"{ID},{CLIENT}")
+    print("Wysłano ID.")
+
+
+with open(CLIENT, "r") as fifo:
+    result = fifo.read().strip()
+    print("Odpowiedź serwera:", result)
--- zad04/serwer.py
+++ zad04/serwer.py
@@ -2,21 +2,41 @@ import os
 import errno
 import time
 
-FIFO_PATH = "/tmp/kolejka"
+SERVER = "/tmp/kolejka"
 
-postac1 = set(1, 'Kowalski')
-postac2 = set(2, 'Nowak')
-postac3 = set(3, 'Witkowska')
-postac4 = set(4, 'Szymanski')
+try:
+    os.mkfifo(SERVER)
+    print('FIFO serwera utworzone.')
+except OSError as oe:
+    if oe.errno != errno.EEXIST:
+        raise
+
+postac1 = {'ID': '1', 'Nazwisko': 'Kowalski'}
+postac2 = {'ID': '2', 'Nazwisko': 'Nowak'}
+postac3 = {'ID': '3', 'Nazwisko': 'A'}
+postac4 = {'ID': '4', 'Nazwisko': 'B'}
 
 baza_danych = [postac1, postac2, postac3, postac4]
 
-while True:
-    try:
-        os.mkfifo(FIFO_PATH)
-    except OSError as oe:
-        if oe.errno != errno.EEXIST:
-            raise
+def find(ID):
+    print("Szukam postaci o ID:", ID)
+    for character in baza_danych:
+        if character['ID'] == ID:
+            return character['Nazwisko']
+    return "Nie ma."
 
-    with open(FIFO_PATH, "w") as fifo:
-        print(fifo)
+while True:
+    with open(SERVER, "r") as fifo:
+        data = fifo.read().strip()
+
+        args = data.split(",")
+        ID, CLIENT = args[0], args[1]
+        print("Serwer otrzymał od klienta ID:", ID)
+
+    result = find(ID)
+
+    with open(CLIENT, "w") as fifo:
+        fifo.write(result)
+    
+    print("Serwer wysłał odpowiedź.")
+    time.sleep(0.5)

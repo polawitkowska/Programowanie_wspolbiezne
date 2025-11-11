7acdec6d1b04850b193375e2d2b15a3731b5894d
Polcia <witkowska.pola@gmail.com>
2025-11-11 19:07:57 +0100

    Refactor client and server communication using structured messages and improve signal handling

--- zad04/klient.py
+++ zad04/klient.py
@@ -1,10 +1,10 @@
 import os
 import errno
-import time
-
+import struct
 
 SERVER = "/tmp/kolejka"
 CLIENT = "/tmp/klient"
+client_path = CLIENT.encode("utf-8")
 
 try:
     os.mkfifo(CLIENT)
@@ -13,13 +13,15 @@ except OSError as oe:
     if oe.errno != errno.EEXIST:
         raise
 
-ID = input("Podaj ID, które chcesz wyszukać: ")
+ID = input("Podaj ID, które chcesz wyszukać: ").encode("utf-8")
+msg = struct.pack("!II", 4 + len(client_path), int(ID)) + client_path
 
-with open(SERVER, "w") as fifo:
-    fifo.write(f"{ID},{CLIENT}")
-    print("Wysłano ID.")
+fd = os.open(SERVER, os.O_WRONLY)
+os.write(fd, msg)
+os.close(fd)
 
+fd_client = os.open(CLIENT, os.O_RDONLY)
+response = os.read(fd_client, 1024).decode("utf-8")
+os.close(fd_client)
 
-with open(CLIENT, "r") as fifo:
-    result = fifo.read().strip()
-    print("Odpowiedź serwera:", result)
+print("Odpowiedź serwera:", response)
--- zad04/serwer.py
+++ zad04/serwer.py
@@ -2,22 +2,23 @@ import os
 import errno
 import time
 import signal
-import sys
+import struct
 
+#variables
+SERVER = "/tmp/kolejka"
+PID = os.getpid()
 running = True
 
+#signal handling
 def handle_usr1(signum, frame):
     global running
     print("\nOtrzymano SIGUSR1: serwer kończy działanie.")
     running = False
-
 signal.signal(signal.SIGHUP, signal.SIG_IGN)
 signal.signal(signal.SIGTERM, signal.SIG_IGN)
 signal.signal(signal.SIGUSR1, handle_usr1)
 
-SERVER = "/tmp/kolejka"
-PID = os.getpid()
-
+#write pid to file and create fifo
 with open("./serwer_pid.txt", "w") as file:
     file.write(f"{PID}")
 print("PID serwera to:", PID)
@@ -29,13 +30,14 @@ except OSError as oe:
     if oe.errno != errno.EEXIST:
         raise
 
-postac1 = {'ID': '1', 'Nazwisko': 'Kowalski'}
-postac2 = {'ID': '2', 'Nazwisko': 'Nowak'}
-postac3 = {'ID': '3', 'Nazwisko': 'A'}
-postac4 = {'ID': '4', 'Nazwisko': 'B'}
-
+#create an example of a database
+postac1 = {'ID': 1, 'Nazwisko': 'Kowalski'}
+postac2 = {'ID': 2, 'Nazwisko': 'Nowak'}
+postac3 = {'ID': 3, 'Nazwisko': 'A'}
+postac4 = {'ID': 4, 'Nazwisko': 'B'}
 baza_danych = [postac1, postac2, postac3, postac4]
 
+#find by id in database
 def find(ID):
     print("Szukam postaci o ID:", ID)
     for character in baza_danych:
@@ -43,18 +45,28 @@ def find(ID):
             return character['Nazwisko']
     return "Nie ma."
 
+#server
 while running:
-    with open(SERVER, "r") as fifo:
-        data = fifo.read().strip()
+    fd = os.open(SERVER, os.O_RDONLY)
+    header = os.read(fd, 8) #only read header first
 
-        args = data.split(",")
-        ID, CLIENT = args[0], args[1]
-        print("Serwer otrzymał od klienta ID:", ID)
+    if not header:
+        os.close(fd)
+        continue
 
+    length, ID = struct.unpack("!II", header)
+    client_path = os.read(fd, length - 4).decode("utf-8")
+    os.close(fd)
+
+    print(f"Otrzymano ID={ID}.")
     result = find(ID)
 
-    with open(CLIENT, "w") as fifo:
-        fifo.write(result)
+    payload = result.encode("utf-8")
+    msg = struct.pack("!I", len(payload)) + payload
+    
+    fd_client = os.open(client_path, os.O_WRONLY)
+    os.write(fd_client, msg)
+    os.close(fd_client)
     
     print("Serwer wysłał odpowiedź.")
     time.sleep(0.5)
